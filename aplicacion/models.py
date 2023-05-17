6f7a5360c475a8cc4a8dff2e46326dfa63e2c14f
Boris Villagran <borisgonzalovillagran@gmail.com>
2023-05-17 14:33:20 -0600

    'modelos'

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Usuario(models.Model):
-    idusuario = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=100, unique=True)
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    correo = models.CharField(max_length=100)
+class User(AbstractUser):
+    pass # Django ya incluye campos para el manejo de usuarios
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
 
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    duration_minutes = models.IntegerField()
+    description = models.TextField()
 
+class MovieGenre(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+class UserMovie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    watched = models.BooleanField(default=False)
+    favorite = models.BooleanField(default=False)
 
 
