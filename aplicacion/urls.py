036e664d07eb471a5fa605b31147290ab0ed95cc
Charlos60 <eduardo123_car@hotmail.com>
2023-05-17 20:24:29 -0600

    login,registro

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -4,7 +4,10 @@ from django.urls import include
 
 
 urlpatterns = [
-    path('', views.home, name='Home'),
+    path('', views.home, name='login'),
+    path('salir',views.salir, name='salir'),
+    path('home', views.home, name='home'),
     path('accounts/',include ('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout 
+from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
+
 
 
 # Create your views here.
@@ -9,5 +13,33 @@ def home(request):
     print(usuario)
     return render(request,'Home/home.html')
 
+def salir(request):
+    logout(request)
+    return redirect('/')
 
     
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        correo = request.POST['correo']
+
+        # Crear un nuevo objeto de usuario
+        user = User.objects.create_user(username=username, password=password)
+
+        # Actualizar los campos personalizados del usuario
+        user.first_name = nombre
+        user.last_name = apellido
+        user.email = correo
+
+        # Guardar el usuario en la base de datos
+        user.save()
+
+        # Redireccionar a la página de inicio de sesión
+        return redirect('/')
+
+    return render(request,'registration/register.html')
