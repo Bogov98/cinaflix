5896f286acf0f7e0c39b979b81e500041a4d7ab7
Dloci <126207779+Dloci@users.noreply.github.com>
2023-05-18 21:53:32 -0600

    nene duende

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    vote_average = models.DecimalField(max_digits=3, decimal_places=1)
+    release_date = models.DateField()
+    imdb_id = models.CharField(max_length=100)
+    generos = models.CharField(max_length=200)
+    poster_link = models.URLField()
+
+    def _str_(self):
+        return self.title
+
 # Create your models here.
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from.models import Movie
 
-# Create your views here.
+def menu(request):
+    movies=Movie.objects.all() [:10]
+    return render(request,'menu/menupeliculas.html',{'movies':movies})
+
+
+
