e0500df1390f511b3e2cc702b847a491f1ddfa6e
Dloci <126207779+Dloci@users.noreply.github.com>
2023-05-23 12:24:57 -0600

    ya se ven las fotos

--- cinaflix/settings.py
+++ cinaflix/settings.py
@@ -130,13 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    'aplicacion/static/css',
-    'aplicacion/static/js',
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'conaflix/static'),)
     
     
     
-]
+    
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
