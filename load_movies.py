d8768b7f0ee5e8600f6579884a4511b5088ccdb7
Bogov98 <83102231+Bogov98@users.noreply.github.com>
2023-05-24 19:24:01 -0600

    arreglos

--- load_movies.py
+++ load_movies.py
@@ -1,22 +1,16 @@
-from django.core.exceptions import ObjectDoesNotExist
-from aplicacion.models import Usuario, Movie, Vista  
-import pandas as pd
+import csv
+from aplicacion.models import Movie
 
-def load_csv_into_db(csv_file_path):
-    data = pd.read_csv(csv_file_path)
-    
-    for index, row in data.iterrows():
-        user_id = row['userId']
-        movie_id = row['movieId']
-        rating = row['rating']
-        
-        try:
-            usuario = Usuario.objects.get(idusuario=user_id)
-            movie = Movie.objects.get(id=movie_id)
-        except ObjectDoesNotExist:
-            print(f"El usuario con ID {user_id} o la película con ID {movie_id} no existen en la base de datos. Se omite esta fila.")
-            continue
-
-        vista = Vista(idusuario=usuario, idmovie=movie, rating=rating)
-        vista.save()
-
+with open('data_movie.csv', 'r',encoding='utf-8') as f:
+    reader = csv.reader(f)
+    next(reader)  # Saltar la fila de encabezados si existe
+    for row in reader:
+        _, created = Movie.objects.get_or_create(
+            id=row[0],
+            title=row[1],
+            vote_average=row[2],
+            release_date=row[3],
+            imdb_id=row[4],
+            generos=row[5],
+            poster_link=row[6],
+        )
